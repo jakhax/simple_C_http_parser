c6b8fada301495bd06d0824bf2943f045177518e
jak <jackogina60@gmail.com>
2020-12-26 02:06:55 +0300

    functions documentated

--- include/http_response.h
+++ include/http_response.h
@@ -31,29 +31,74 @@ typedef struct http_response {
 
 } http_response_t;
 
-
+/**
+ * @brief allocates and sets defaults on
+ * @return 0 on success and < 0 on error
+ */
 int http_response_init(http_response_t* r);
 
+/**
+ * @brief de-allocates http_response_t items
+ */
 void http_response_free(http_response_t* r);
 
+/**
+ * @brief set response status code and message
+ * @return 0 on success and < 0 on error
+ */
 int http_response_set_status(http_response_t* r, int a_status_code, char* a_status_message, size_t len);
 
+/**
+ * @brief set response url
+ * @return 0 on success and < 0 on error
+ */
 int http_response_set_url(http_response_t* r, const char* url, size_t len);
 
+/**
+ * @brief set response method
+ * @return 0 on success and < 0 on error
+ */
 void http_response_set_method(http_response_t* r, int method);
 
+/**
+ * @brief set response body
+ * @return 0 on success and < 0 on error
+ */
 int http_response_set_body(http_response_t* r, const char* at, uint32_t length);
 
+/**
+ * @brief set response header field
+ * supports chunked
+ * @return 0 on success and < 0 on error
+ */
 int http_response_set_header_field(http_response_t* r, const char* field, size_t field_len);
 
+/**
+ * @brief set response header value
+ * supports chunked header
+ * @return 0 on success and < 0 on error
+ */
 int http_response_set_header_value(http_response_t* r, const char* value, size_t value_len);
 
+/**
+ * @brief set response status code and message
+ * @return 0 on success and < 0 on error
+ */
 int http_response_set_headers_complete(http_response_t* r);
 
+/**
+ * @brief increments bdy_length
+ */
 void http_response_increase_body_length(http_response_t* r, uint32_t length);
 
+/**
+ * @brief set chunked to true
+ */
 void http_response_set_chunked(http_response_t* r);
 
+/**
+ * @brief set message as complete
+ */
 void http_response_set_message_complete(http_response_t* r);
 
 
