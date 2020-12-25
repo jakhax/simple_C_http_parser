ee6298b3278569b198c65fdc8b215cb83aecfcd5
jak <jackogina60@gmail.com>
2020-12-26 01:40:20 +0300

    simple unit tests

---
+++ TESTS/test_common.h
@@ -0,0 +1,11 @@
+#ifndef _C_HTTP_CLIENT_TEST_COMMON_H_
+#define _C_HTTP_CLIENT_TEST_COMMON_H_
+
+#include "common.h"
+
+#define info(x, ...) printf("[TEST][INFO] " x "\r\n", ##__VA_ARGS__)
+#define error(x, ...) printf("[TEST][ERROR] " x "\r\n", ##__VA_ARGS__)
+
+void run_tests(const char* test_name, int (*tests[])(void), int tests_count);
+
+#endif
